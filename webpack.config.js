c383b1cb00f79b4b6c9fcda9374f5a60385d583b
anshul119 <assharmaanshul119@gmail.com>
2018-07-21 13:35:48 +0200

    dev server and hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,51 @@
-module.exports = (environment) => {
-    const isProd = 'production'.indexOf(environment) !== -1;
-    return {
-        entry: './index.tsx',
-        output: {
-            filename: 'bundle.js',
-            path: __dirname + '/dist'
-        },
-        devtool: 'source-map',
-        resolve: {
-            extensions: ['.ts', '.tsx', '.js', '.json']
-        },
-        module: {
-            rules: [
-                { test: /\.tsx?$/, exclude: /node_modules/, loader: "ts-loader" },
-                { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-            ].concat(isProd ? [] : [{ test: /\.tsx?$/, exclude: /node_modules/, loader: "tslint-loader" }])
-        }
-    }
-}
+var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = environment => {
+	const isProd = 'production'.indexOf(environment) !== -1;
+	return {
+		entry:  path.join(__dirname, 'src', 'index.tsx'),
+		output: {
+			filename: 'bundle.js',
+			path: path.join(__dirname, 'dist')
+		},
+		devtool: 'source-map',
+		resolve: {
+			extensions: ['.ts', '.tsx', '.js', '.json']
+		},
+		module: {
+			rules: [
+				{ test: /\.tsx?$/, exclude: /node_modules/, loader: 'ts-loader' },
+				{ enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' }
+			].concat(isProd ? [] : [{ test: /\.tsx?$/, exclude: /node_modules/, loader: 'tslint-loader' }])
+		},
+		plugins: [
+			new HtmlWebpackPlugin(
+				Object.assign(
+					{
+						filename: 'index.html',
+						template: path.join(__dirname, 'src', 'index.html'),
+						minify: isProd
+							? {
+									collapseInlineTagWhitespace: true,
+									collapseWhitespace: true,
+									sortAttributes: true,
+									sortClassName: true
+							  }
+							: false
+					},
+					isProd ? { filename: path.join(__dirname, 'dist', 'index.html') } : null
+				)
+			),
+		],
+		devServer: {
+			contentBase: path.join(__dirname, 'dist'),
+			compress: true,
+			publicPath: '/',
+			port: 8080,
+			stats: {
+				color: true
+			}
+		}
+	};
+};
